f3d79e7df72444dd450fedd01ddc102beed8b7c0
emiel <emielverhoef@hotmail.com>
2021-09-08 15:27:35 +0200

    changed comments

--- 1-recursion-and-lists/src/Lib.hs
+++ 1-recursion-and-lists/src/Lib.hs
@@ -19,48 +19,47 @@ module Lib
     ( ex1, ex2, ex3, ex4, ex5, ex6, ex7
     ) where
 
-{-| The 'ex1' function calculates the sum of a list of integers
+{-| The 'ex1' function calculates the sum of a list of integers using a recursive loop
  it takes 1 argument, of type '[Int]'. It returns type 'Int' -}
 ex1 :: [Int] -> Int
 ex1 (x:xs) = x + ex1 xs
-ex1 [] = 0
+ex1 _ = 0
 
 
-{-| The 'ex2' function increases every integer in a list of integers by one
+{-| The 'ex2' function increases every integer in a list of integers by one and prepends it to a new list in a recursive loop
 it takes 1 argument, of type '[Int]'. It returns type '[Int]'-}
 ex2 :: [Int] -> [Int]
 ex2 (x:xs) = x+1 : ex2 xs
-ex2 [] = []
+ex2 _ = []
 
 
-{-| The 'ex3' function multiplies every integer in a list of integers by minus one
+{-| The 'ex3' function multiplies every integer in a list of integers by minus one and prepends it to a new list in a recursive loop
 it takes 1 argument, of type '[Int]'. It returns type '[Int]' -}
 ex3 :: [Int] -> [Int]
 ex3 (x:xs) = -x : ex3 xs
-ex3 [] = []
+ex3 _ = []
 
 
-{-| The 'ex4' function concatenates 2 lists of integers
+{-| The 'ex4' function concatenates 2 lists of integers by recursively looping through one of the lists and prepending the values to the second list
 it takes 2 arguments, of types '[Int]', '[Int]'. It returns type '[Int]' -}
 ex4 :: [Int] -> [Int] -> [Int]
 ex4 []y = y
 ex4 (x:xs) y = x: ex4 xs y
 
-
 {-| The 'ex5' function looks at the values of two lists of integers (equal in length),
-and returns a list made up of the sums of said values at corresponding indices
+and prepends the sums of said values at corresponding indices to a new list using a recursive loop
 it takes 2 arguments, of types '[Int]', '[Int]'. It returns type '[Int]' -}
 ex5 :: [Int] -> [Int] -> [Int]
 ex5 (x:xs) (y:ys) = x+y: ex5 xs ys
-ex5 [][] = []
+ex5 _ _ = []
 
 
 {-| The 'ex6' function looks at the values of two lists of integers (equal in length),
-and returns a list made up of the products of said values at corresponding indices
+and prepends the products of said values at corresponding indices to a new list using a recursive loop
 it takes 2 arguments, of types '[Int]', '[Int]'. It returns type '[Int]' -}
 ex6 :: [Int] -> [Int] -> [Int]
 ex6 (x:xs) (y:ys) = x*y: ex6 xs ys
-ex6 [][] = []
+ex6 _ _ = []
 
 
 {-| The 'ex7' function calculates the dot product of 2 lists of integers,
